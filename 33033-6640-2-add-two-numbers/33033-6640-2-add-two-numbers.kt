54f284f06e0436f1fe29a721c1f3ad332395915c
sangyoon <57611838+squart300kg@users.noreply.github.com>
2025-03-14 11:25:39 +0900

    Time: 3 ms, Memory: 49.4 MB - LeetHub

--- 33033-6640-2-add-two-numbers/33033-6640-2-add-two-numbers.kt
+++ 33033-6640-2-add-two-numbers/33033-6640-2-add-two-numbers.kt
@@ -10,56 +10,49 @@
 
  class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        val listL1 = listNodeToArray(l1)
-        val listL2 = listNodeToArray(l2)
-        val length = Math.max(listL1.size, listL2.size)
-        var tempResult = IntArray(length + 1)
+        if (l1 == null) return l2
+        if (l2 == null) return l1
+
+        var copiedL1 = l1
+        var copiedL2 = l2
+        var headNode = ListNode(0)
+        var currentNode = ListNode(0)
         var prime = 0
-        var isNeededToExpandArray = false
-        for (index in 0 until length + 1) {
-            if (index <= listL1.lastIndex) {
-                tempResult[index] = listL1[index]
+        var isFirstLoop = true
+        while (copiedL1?.`val` != null || copiedL2?.`val` != null || prime != 0) {
+            if (copiedL1?.`val` != null) {
+                currentNode.`val` = copiedL1.`val`
             }
-            if (index <= listL2.lastIndex) {
-                tempResult[index] += listL2[index]
+            if (copiedL2?.`val` != null) {
+                currentNode.`val` += copiedL2.`val`
             }
 
-            if (index == length && prime != 0) isNeededToExpandArray = true
-
-            tempResult[index] += prime
-            if (tempResult[index] >= 10) prime = tempResult[index] / 10
+            currentNode.`val` += prime
+            if (currentNode.`val` >= 10) prime = currentNode.`val` / 10
             else prime = 0
-            tempResult[index] %= 10
-        }
+            currentNode.`val` %= 10
 
-        if (!isNeededToExpandArray) {
-            tempResult = IntArray(length) {
-                tempResult[it]
+            if (isFirstLoop) {
+                headNode = currentNode
+                isFirstLoop = false
+            }
+
+            copiedL1 = copiedL1?.next
+            copiedL2 = copiedL2?.next
+            when {
+
+                copiedL1?.`val` != null || copiedL2?.`val` != null -> {
+                    currentNode.next = ListNode(0)
+                    currentNode.next?.let { currentNode = it }
+                }
+                else -> {
+                    if (prime != 0) {
+                        currentNode.next = ListNode(0)
+                        currentNode.next?.let { currentNode = it }
+                    }
+                }
             }
         }
-        return arrayToListNode(tempResult)
+        return headNode
     }
-}
-
-
-// 배열을 연결 리스트로 변환하는 함수
-fun arrayToListNode(arr: IntArray): ListNode? {
-    if (arr.isEmpty()) return null
-    val head = ListNode(arr[0])
-    var current = head
-    for (i in 1 until arr.size) {
-        current.next = ListNode(arr[i])
-        current = current.next!!
-    }
-    return head
-}
-
-fun listNodeToArray(node: ListNode?): IntArray {
-    val result = mutableListOf<Int>()
-    var current = node
-    while (current != null) {
-        result.add(current.`val`)
-        current = current.next
-    }
-    return result.toIntArray()
 }
